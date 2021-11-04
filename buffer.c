3c64baa1fa898202e293b27934024583f6f1beca
Michael Anderson <trenonian@gmail.com>
2021-11-04 18:16:39 -0400

    first pass

--- buffer.c
+++ buffer.c
@@ -9,6 +9,8 @@
 typedef int buffer_item;
 #define BUFFER_SIZE 8
 
+int USE_MICROSECONDS = 1;
+
 char FIRST_INITIAL = 'm';
 char *LAST_NAME = "anderson";
 
@@ -25,18 +27,17 @@ buffer_item buffer[BUFFER_SIZE];
 
 pthread_mutex_t mutex;
 pthread_mutex_t value_mutex;
-pthread_mutex_t print_mutex;
 sem_t empty;
 sem_t full;
 
-void printBuffer();
+int insertPointer = 0, removePointer = 0;
 
 void *producer(void *param);
 void *consumer(void *param);
 
-int insert_item(buffer_item item);
+int insert_item(buffer_item item, int index);
 
-int remove_item(buffer_item *item);
+int remove_item(buffer_item *item, int index);
 
 int main(int argc, char *argv[])
 {
@@ -61,8 +62,6 @@ int main(int argc, char *argv[])
 	consumerThreads = atoi(argv[3]);
 	START_NUMBER = atoi(argv[4]);
 
-	//printf("top of main\n");
-
 	/* Initialize the synchronization tools */
 	if (pthread_mutex_init(&mutex, NULL) != 0)
 	{
@@ -74,24 +73,15 @@ int main(int argc, char *argv[])
 		printf("mutex init has failed\n");
 		return 1;
 	}
-	if (pthread_mutex_init(&print_mutex, NULL) != 0)
-	{
-		printf("mutex init has failed\n");
-		return 1;
-	}
 	sem_init(&empty, 1, BUFFER_SIZE);
 	sem_init(&full, 1, 0);
-
-	//printf("inits done\n");
 
 	produce_value = START_NUMBER;
 
 	/* Create the producer and consumer threads */
-	//printf("creating producers\n");
 	pthread_t producers[producerThreads];
 	for (int i = 0; i < producerThreads; i++)
 	{
-		//printf("creating producer %d\n", i);
 		parameter *data = (parameter *)malloc(sizeof(parameter));
 		data->index = i;
 		pthread_create(&producers[i], NULL, producer, data);
@@ -105,14 +95,26 @@ int main(int argc, char *argv[])
 		pthread_create(&consumers[i], NULL, consumer, data);
 	}
 
-	/* Sleep for user specified time */
-	sleep(sleepTime);
-	// struct timespec ts;
-	// ts.tv_sec = (sleepTime / 1000);
-	// ts.tv_nsec = (sleepTime % 1000);
-	// nanosleep(&ts, &ts);
+	if (USE_MICROSECONDS == 0)
+	{
+		/* Sleep for user specified number of seconds */
+		sleep(sleepTime * 1000000);
+	}
+	else
+	{
+		/* Sleep for user specified number of milliseconds */
+		usleep(sleepTime);
+	}
 
-	// printf("done");
+	sem_destroy(&empty);
+	sem_destroy(&full);
+
+	if (pthread_mutex_lock(&mutex) != 0)
+	{
+		return -1;
+	}
+
+	printf("DONE\n");
 
 	return 0;
 }
@@ -123,28 +125,17 @@ void *producer(void *param)
 
 	parameter *data = (parameter *)param;
 	int index = data->index;
-	buffer_item item;
 	free(data);
+	buffer_item item;
 	while (TRUE)
 	{
+		/* generate/retrieve item */
 		pthread_mutex_lock(&value_mutex);
 		item = produce_value++;
+
+		/* insert item */
+		insert_item(item, index);
 		pthread_mutex_unlock(&value_mutex);
-		// printf("producer_%d with item %d\n", index, item);
-		if (insert_item(item))
-		{
-			pthread_mutex_lock(&print_mutex);
-			printf("error producing item %d\n", item);
-			pthread_mutex_unlock(&print_mutex);
-		}
-		else
-		{
-			pthread_mutex_lock(&print_mutex);
-			printf("Producer %c%s_%d produced %d\n",
-				   FIRST_INITIAL, LAST_NAME, index, item);
-			pthread_mutex_unlock(&print_mutex);
-		}
-		// sleep(1);
 	}
 }
 
@@ -153,54 +144,53 @@ void *consumer(void *param)
 	/* Implementation of the consumer thread -- refer to Figure 5.26 on page 256 */
 	parameter *data = (parameter *)param;
 	int index = data->index;
-	buffer_item item;
 	free(data);
-
-	// printf("consumer_%d\n", index);
+	buffer_item item;
 	while (TRUE)
 	{
-		if (remove_item(&item))
-		{
-			pthread_mutex_lock(&print_mutex);
-			printf("error consuming item %d\n", item);
-			pthread_mutex_unlock(&print_mutex);
-		}
-		else
-		{
-			pthread_mutex_lock(&print_mutex);
-			printf("Consumer %c%s_%d comsumed %d\n",
-				   FIRST_INITIAL, LAST_NAME, index, item);
-			pthread_mutex_unlock(&print_mutex);
-		}
-		// sleep(1);
+		remove_item(&item, index);
+		/* process item */
 	}
 }
 
-int insert_item(buffer_item item)
+int insert_item(buffer_item item, int index)
 {
 	/*
 		insert item into buffer
 		return 0 if successful
 		return -1 if not successful
 	*/
-	// printf("waiting to insert %d\n", item);
+
+	// if (sem_wait(&empty) != 0)
+	// {
+	// 	return -1;
+	// }
+	// if (pthread_mutex_lock(&mutex) != 0)
+	// {
+	// 	return -1;
+	// }
 	sem_wait(&empty);
 	pthread_mutex_lock(&mutex);
-	int *idx;
-	sem_getvalue(&full, idx);
-	// printf("inserting %d at [%d]\n", item, *idx);
-	buffer[*idx] = item;
-	// printBuffer();
+
+	buffer[insertPointer] = item;
+	insertPointer++;
+	insertPointer %= BUFFER_SIZE;
+	printf("Producer %c%s_P%d produced %d\n",
+		   FIRST_INITIAL, LAST_NAME, index, item);
+	// if (sem_post(&full) != 0)
+	// {
+	// 	return -1;
+	// }
+	// if (pthread_mutex_unlock(&mutex) != 0)
+	// {
+	// 	return -1;
+	// }
 	sem_post(&full);
-	// int *e, *f;
-	// sem_getvalue(&empty, e);
-	// sem_getvalue(&full, f);
-	// printf("%d:%d\n", *e, *f);
 	pthread_mutex_unlock(&mutex);
 	return 0;
 }
 
-int remove_item(buffer_item *item)
+int remove_item(buffer_item *item, int index)
 {
 	/*
 		remove object from buffer
@@ -208,27 +198,31 @@ int remove_item(buffer_item *item)
 		return 0 if successful
 		return -1 if not successful
 	*/
-	// printf("waiting to remove\n");
+	// if (sem_wait(&full) != 0)
+	// {
+	// 	return -1;
+	// }
+	// if (pthread_mutex_lock(&mutex) != 0)
+	// {
+	// 	return -1;
+	// }
 	sem_wait(&full);
 	pthread_mutex_lock(&mutex);
-	int *idx;
-	sem_getvalue(&full, idx);
-	// printf("consuming [%d]\n", *idx);
-	*item = buffer[*idx];
-	// printBuffer();
-	// printf("removing %d from [%d]\n", *item, *idx);
+	*item = buffer[removePointer];
+	removePointer++;
+	removePointer %= BUFFER_SIZE;
+	printf("Consumer %c%s_C%d comsumed %d\n",
+		   FIRST_INITIAL, LAST_NAME, index, *item);
+	// if (sem_post(&empty) != 0)
+	// {
+	// 	return -1;
+	// }
+	// if (pthread_mutex_unlock(&mutex) != 0)
+	// {
+	// 	return -1;
+	// }
 	sem_post(&empty);
 	pthread_mutex_unlock(&mutex);
 
 	return 0;
-}
-
-void printBuffer()
-{
-	printf("%d", buffer[0]);
-	for (int i = 1; i < BUFFER_SIZE; i++)
-	{
-		printf(",%d", buffer[i]);
-	}
-	printf("\n");
 }
